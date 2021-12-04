9c8ce5f796c8a6a6c4b39435e4be6a1b4806e515
yeeun0514 <yeeun.g.sin@gmail.com>
2021-12-04 17:09:58 +0900

    [#1] type : git clone push pull test

--- src/main/java/com/amazing/todolist/AmazingTodolistApplication.java
+++ src/main/java/com/amazing/todolist/AmazingTodolistApplication.java
@@ -8,6 +8,8 @@ public class AmazingTodolistApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AmazingTodolistApplication.class, args);
+
+        
     }
 
 }
