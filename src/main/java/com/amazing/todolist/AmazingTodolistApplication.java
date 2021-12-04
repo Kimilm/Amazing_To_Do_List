c92400063ae86b214d2c99c78ae568f6be6fb511
kimilm <gnlfla7416@gmail.com>
2021-12-04 16:29:56 +0900

    create repository

---
+++ src/main/java/com/amazing/todolist/AmazingTodolistApplication.java
@@ -0,0 +1,13 @@
+package com.amazing.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazingTodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazingTodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amazing/todolist/AmazingTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazing.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingTodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
