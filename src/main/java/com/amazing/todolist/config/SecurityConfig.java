683d8163a9671b07805b0d3f690200cb920ca23a
kimilm <gnlfla7416@gmail.com>
2022-01-01 15:44:30 +0900

    [#3] refactor: remove default login form

---
+++ src/main/java/com/amazing/todolist/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.amazing.todolist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // 기본 로그인폼 제거
+        httpSecurity.httpBasic().disable();
+    }
+}
